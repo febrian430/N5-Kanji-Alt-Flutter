a952436115635fa53e83813519557fd5df298336
Febrian <febrian430@gmail.com>
2022-01-03 12:05:05 +0700

    color gradation POC

--- lib/test.dart
+++ lib/test.dart
@@ -2,12 +2,21 @@ import 'dart:async';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:kanji_memory_hint/audio_repository/audio.dart';
+import 'package:kanji_memory_hint/color_hex.dart';
 import 'package:kanji_memory_hint/countdown.dart';
-import 'package:quiver/async.dart';
+import 'package:kanji_memory_hint/database/kanji.dart';
+import 'package:kanji_memory_hint/database/repository.dart';
+import 'package:kanji_memory_hint/database/user_point.dart';
 
 
 
 class Test extends StatefulWidget {
+  
+  Future<UserPoint> getUserPoints() async {
+    return SQLRepo.userPoints.get();
+  }
+  
   @override
   State<StatefulWidget> createState() => _TestState();
 }
@@ -17,10 +26,19 @@ class _TestState extends State<Test> {
     countdown = Countdown(onTick: onCountdownTick, onDone: onCountdownOver, startTime: 10,);
   }
 
-  bool paused = true;
+  var points;
 
+  bool paused = true;
   int currentTime = 10;
+
   late Countdown countdown;
+
+  @override
+  void initState() {
+    super.initState();
+
+    points = widget.getUserPoints();
+  }
 
   void onCountdownTick(int current) {
     print("TICK TOCK");
@@ -32,26 +50,116 @@ class _TestState extends State<Test> {
   void onCountdownOver() {
     print("GAME OVER");
   }
- 
-  
   
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            Text("test"),
-            Text(currentTime.toString()),
-            ElevatedButton(
-              onPressed: () {
-                countdown.toggle();
-              },
-            child: Text("toggle pause"),
-          ),
-          ],
+        child: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter, // 10% of the width, so there are ten blinds.
+                colors: <Color>[
+                    HexColor.fromHex("f9e7b1"),
+                    HexColor.fromHex("f9e7b1"),
+                    HexColor.fromHex("f8b444")
+                ],
+                tileMode: TileMode.decal, // repeats the gradient over the canvas
+              ),
+            ),
+          child: Column(
+            children: [
+              Text("test"),
+              Text(currentTime.toString()),
+              ElevatedButton(
+                onPressed: () {
+                  countdown.toggle();
+                },
+                child: Text("toggle pause"),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  SelectAudio.play();
+                },
+                child: Text("do sound"),
+              ),
+
+              Flexible(
+                flex: 3,
+                child: FutureBuilder(
+                  future: points,
+                  builder: (BuildContext context, AsyncSnapshot<UserPoint> snapshot){
+                    if(snapshot.hasData) {
+                      return Column(
+                        children: [
+                          Text('EXP: ${snapshot.data!.exp}'),
+                          Text('Gold: ${snapshot.data!.gold}'),
+                          ElevatedButton(
+                            child: Text("Add exp and point"),
+                            onPressed: () {
+                              SQLRepo.userPoints.addExp(50);
+                              setState(() {
+                                points = widget.getUserPoints();
+                              });
+                            },
+                          ),
+                        ],
+                      );
+                    } else {
+                      return Text("Loading");
+                    }
+                  }
+                )
+              ),
+
+              Flexible(
+                flex: 3,
+                  child: FutureBuilder(
+                  future: SQLRepo.kanjis.all(),
+                  builder: (context, AsyncSnapshot<List<Kanji>> snapshot) {
+                    if(snapshot.hasData) {
+                      return ListView(
+                        children: snapshot.data!.map((kanji) => KanjiWidget(kanji: kanji)).toList(),
+                      );
+
+                    } else {
+                      return Text("Loading");
+                    }
+                  },
+                )
+              )
+            ],
+          )
         )
       )
+    );
+  }
+}
+
+class KanjiWidget extends StatelessWidget {
+
+  final Kanji kanji;
+
+  const KanjiWidget({Key? key, required this.kanji}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(
+          width: 1
+        )
+      ),
+      child: Column(
+        children: [
+          Text(kanji.id.toString()),
+          Text(kanji.rune),
+          Text(kanji.kunyomi.join("/")),
+          Text(kanji.onyomi.join("/")),
+          Text(kanji.chapter.toString()),
+          Text(kanji.mastery.toString()),
+        ],
+      ),
     );
   }
 }
