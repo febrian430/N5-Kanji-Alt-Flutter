4ec083a9b01f4e0b44a2d85e152761733cb54a4a
Febrian <febrian430@gmail.com>
2022-01-04 13:54:15 +0700

    image on menu button POC

--- lib/components/buttons/select_button.dart
+++ lib/components/buttons/select_button.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:kanji_memory_hint/color_hex.dart';
-import 'package:kanji_memory_hint/components/buttons/material_state_button.dart';
+import 'package:kanji_memory_hint/const.dart';
 import 'package:kanji_memory_hint/theme.dart';
+
 
 class SelectButton extends StatelessWidget {
   final String title;
@@ -10,7 +10,6 @@ class SelectButton extends StatelessWidget {
   final Function() onTap;
 
   SelectButton({Key? key, required this.title, this.description, required this.onTap}) : super(key: key) {
-    _buttonColor = AppButtonTheme.buttonColor;
     _titleTextColor = Colors.black;
     _descTextColor = Colors.grey;
   }
@@ -18,7 +17,6 @@ class SelectButton extends StatelessWidget {
   static const double _titleFontSize = 20;
   static const double _descFontSize = 14;
 
-  late Color _buttonColor;
   late Color _titleTextColor;
   late Color _descTextColor;
 
@@ -53,13 +51,22 @@ class SelectButton extends StatelessWidget {
     final size = MediaQuery.of(context).size;
 
     Widget child = Center(
-            child: Text(
-              title,
-              style: TextStyle(
-                fontSize: _titleFontSize,
-                color: _titleTextColor
-              ),
-            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text(
+                  title,
+                  style: Theme.of(context).textTheme.button,
+                ),
+                Image(
+                  image: AssetImage(APP_IMAGE_FOLDER+'sakura.png'), 
+                  height: 30,
+                  width: 30,
+                  fit: BoxFit.fill,
+                ),
+              ],
+            ) 
+            
           );
     if (description != null) {
       child = _withDescription(context, child);
@@ -72,12 +79,11 @@ class SelectButton extends StatelessWidget {
         constraints: BoxConstraints(
           minHeight: size.height*0.075
         ),
-        width: size.width*0.4,
+        width: size.width*0.40,
         decoration: BoxDecoration(
           border: Border.all(
             width: 2
           ),
-          color: _buttonColor,
         ),
       )
     );
