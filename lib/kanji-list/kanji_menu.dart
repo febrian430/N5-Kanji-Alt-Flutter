26279988ae2bc8300708fd764c5ee9a095140aa4
Febrian <febrian430@gmail.com>
2022-01-06 21:32:32 +0700

    only one expanded kanji list

--- lib/kanji-list/kanji_menu.dart
+++ lib/kanji-list/kanji_menu.dart
@@ -3,7 +3,11 @@ import 'package:flutter/material.dart';
 import 'package:kanji_memory_hint/components/loading_screen.dart';
 import 'package:kanji_memory_hint/database/kanji.dart';
 import 'package:kanji_memory_hint/database/repository.dart';
+import 'package:kanji_memory_hint/kanji-list/tile.dart';
 import 'package:kanji_memory_hint/menu_screens/menu.dart';
+import 'package:kanji_memory_hint/theme.dart';
+import 'package:kanji_memory_hint/map_indexed.dart';
+
 
 class KanjiMenu extends StatefulWidget {
   KanjiMenu({Key? key}) : super(key: key);
@@ -24,6 +28,8 @@ class _MenuState extends State<KanjiMenu> {
   @override
 
   var _list;
+
+  int selected = -1;
 
   @override
   void initState() {
@@ -47,48 +53,70 @@ class _MenuState extends State<KanjiMenu> {
   }
 
   Widget _renderList(BuildContext context, List<Kanji> kanjis) {
-    return SingleChildScrollView(
-      child: Center(
-        child: Column(
-          children: 
-            widget.chapters.map((chapter) {
-              return ExpansionTile(
-                textColor: Colors.black,
-                backgroundColor: Colors.transparent,
-                childrenPadding: const EdgeInsets.all(4),
-                expandedCrossAxisAlignment: CrossAxisAlignment.end,
-                children: <Widget>[
-                  GridView.count(
-                    crossAxisCount: 5,
-                    padding: const EdgeInsets.all(4),
-                    crossAxisSpacing: 12,
-                    mainAxisSpacing: 12,
-                    physics: const NeverScrollableScrollPhysics(),
-                    shrinkWrap: true,
-                    children: kanjis.where((kanji) => kanji.chapter == chapter)
-                      .map((kanji) {
-                        return buildKanji(context, kanji);
-                      }).toList() 
+    final screenWidth = MediaQuery.of(context).size.width;
+    final childWidth = screenWidth*0.8;
+    final width = screenWidth*0.6;
+
+    return ListView.builder(
+            key: Key('builder ${selected.toString()}'),
+            itemCount: widget.chapters.length,
+            itemBuilder: (context, index) {
+              var chapter = widget.chapters[index];
+              return KanjiTile(
+                  key: Key(index.toString()),
+                  initiallyExpanded: selected == index,
+                  onExpansionChanged: (expanding) {
+                    if(expanding){
+                      setState(() {
+                        selected = index;
+                      });
+                    } else {
+                      setState(() {
+                        selected = -1;
+                      });
+                    }
+                  },
+                  textColor: Colors.black,
+                  childrenPadding: const EdgeInsets.all(4),
+                  expandedCrossAxisAlignment: CrossAxisAlignment.end,
+                  width: width,
+                  childWidth: childWidth,
+                  headerBackgroundColor: AppColors.primary,
+                  headerBorderWidth: 3,
+                  
+                  children: <Widget>[
+                    GridView.count(
+                      crossAxisCount: 5,
+                      padding: const EdgeInsets.all(4),
+                      crossAxisSpacing: 12,
+                      mainAxisSpacing: 12,
+                      physics: const NeverScrollableScrollPhysics(),
+                      shrinkWrap: true,
+                      children: kanjis.where((kanji) => kanji.chapter == chapter)
+                        .map((kanji) {
+                          return buildKanji(context, kanji);
+                        }).toList() 
+                    )
+                  ],
+                  title: Text(
+                      "Chapter ${chapter}"
                   )
-                ],
-                title: Center(
-                  child: Text(
-                    "Chapter ${chapter}"
-                  )
-                )
               );
-            }).toList()
-        ),
-      ),
+            }
     );
   }
 
+  @override
   Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
     final screen = FutureBuilder(
       future: _list,
       builder: (context, AsyncSnapshot<List<Kanji>> snapshot) {
         if(snapshot.hasData) {
-          return _renderList(context, snapshot.data!);
+          return SizedBox(
+            height: height*0.75,
+            child: _renderList(context, snapshot.data!)
+          );
         } else {
           return Center(
             child: LoadingScreen(),
