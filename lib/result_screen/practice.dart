290bdd81f9938915d35cdd271c9594657aed7354
Febrian <febrian430@gmail.com>
2022-01-09 10:55:47 +0700

    practice result screen

--- lib/result_screen/practice.dart
+++ lib/result_screen/practice.dart
@@ -2,8 +2,12 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:kanji_memory_hint/components/backgrounds/menu_background.dart';
 import 'package:kanji_memory_hint/components/backgrounds/practice_background.dart';
+import 'package:kanji_memory_hint/components/buttons/icon_button.dart';
+import 'package:kanji_memory_hint/icons.dart';
 import 'package:kanji_memory_hint/main.dart';
+import 'package:kanji_memory_hint/menu_screens/menu.dart';
 import 'package:kanji_memory_hint/route_param.dart';
+import 'package:kanji_memory_hint/theme.dart';
 
 class ResultScreen extends StatelessWidget{
   // ResultScreen({required this.wrongCount, required this.decreaseFactor}){
@@ -18,83 +22,160 @@ class ResultScreen extends StatelessWidget{
   // final int decreaseFactor = 0;
   // late int points = 0;
 
+  Widget _rowOfButtons(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(6),
+      child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        AppIconButton(
+          onTap: (){}, 
+          iconPath: AppIcons.resume, 
+          height: 50, 
+          width: 50, 
+          backgroundColor: AppColors.primary
+        ),
+        AppIconButton(
+          onTap: (){Navigator.of(context).popUntil(ModalRoute.withName("/game"));}, 
+          iconPath: AppIcons.viewResult, 
+          height: 50, 
+          width: 50, 
+          backgroundColor: AppColors.primary
+        ),
+        AppIconButton(
+          onTap: (){Navigator.of(context).popUntil(ModalRoute.withName("/game"));}, 
+          iconPath: AppIcons.reminderSmall, 
+          height: 50, 
+          width: 50, 
+          backgroundColor: AppColors.primary
+        ),
+        AppIconButton(
+          onTap: (){Navigator.of(context).popUntil(ModalRoute.withName("/game"));}, 
+          iconPath: AppIcons.exit, 
+          height: 50, 
+          width: 50, 
+          backgroundColor: AppColors.wrong
+        )
+      ],
+      )
+    );
+  }
+
+  Widget _screen(BuildContext context, ResultParam param, Stopwatch stopwatch) {
+    final size = MediaQuery.of(context).size;
+
+    return Container(
+      width: size.width*0.75,
+      height: size.height*0.60,
+      decoration: BoxDecoration(
+        border: Border.all(
+          width: 1
+        ),
+        color: AppColors.primary
+      ),
+      child: Padding(
+        padding: EdgeInsets.all(6),
+        child: Container(
+          decoration: BoxDecoration(
+            border: Border.all(
+              width: 1
+            )
+          ),
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(flex: 2, child: Center(child: Text("CONGRATS", style: TextStyle(fontSize: 36)))),
+          Expanded(flex: 6, child: _DetailWidget(param: param, stopwatch: stopwatch)),
+          Flexible(flex: 1, child: _rowOfButtons(context))
+        ]
+          )
+          )
+      )
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     var param = ModalRoute.of(context)!.settings.arguments as ResultParam;
     var stopwatch = param.stopwatch;
+    stopwatch.stop();
+    final screen = _screen(context, param, stopwatch);
 
-    return MenuBackground(
-      child: Scaffold(
-        body: SafeArea(
-          child: Center(
-            child: Container(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.center,
+    return Menu(title: "Result", japanese: "result in japanese", child: screen);
+  }
+}
 
-                children: [
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text("Wrong attempts"),
-                      Text(param.score.wrongAttempts.toString()),
-                      SizedBox(height: 100,),
+class _DetailWidget extends StatelessWidget {
 
-                      Text("Perfect rounds"),
-                      Text(param.score.perfectRounds.toString()),
-                      SizedBox(height: 100,),
+  final ResultParam param;
+  final Stopwatch stopwatch;
 
-                      Text("Points gained"),
-                      Text(param.result.pointsGained.toString()),
-                      SizedBox(height: 100,),
+  const _DetailWidget({Key? key, required this.param, required this.stopwatch}) : super(key: key);
 
-                      Text("Exp gained"),
-                      Text(param.result.expGained.toString()),
-                      SizedBox(height: 100,),
+  String humanize(Stopwatch stopwatch) {
+    var elapsed = stopwatch.elapsed;
+    var seconds = elapsed.inSeconds;
 
-                      Text("Time elapsed"),
-                      Text(stopwatch.elapsed.toString())
-                  ]
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
+    var minutes = (seconds/60).floor();
+    var remaining = seconds % 60;
+    return "${minutes > 0 ?  minutes.toString() + " min" : ""}$remaining seconds";
+  }
 
-                    children: [
-                      TextButton(
-                        onPressed: () {
-                          // Navigator.of(context).pushAndRemoveUntil(
-                          //   MaterialPageRoute(
-                          //     builder: (context) => Home()
-                          //   ),
-                          //   (route) => false
-                          // );
-                          Navigator.of(context).popUntil(ModalRoute.withName("/"));
-                        },
-                        child: Center(
-                          child: Text(
-                            "Main Screen"
-                          ),
-                        ),
-                      ),
-                      TextButton(
-                        onPressed: () {
-                          Navigator.of(context).popUntil(ModalRoute.withName("/game"));
-                        },
-                        child: Center(
-                          child: Text(
-                            "Practice"
-                          ),
-                        ),
-                      ),
-                    ]
-                  )
-                ],
-              )
-            )
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        RichText(
+          text: TextSpan(
+            style: new TextStyle(
+              fontSize: 14.0,
+              color: Colors.black,
+            ),
+            children: <TextSpan>[
+              TextSpan(
+                text: param.result.pointsGained.toString(),
+                style: TextStyle(
+                  fontSize: 70,
+                  color: Colors.amber
+                  
+                ),
+              ),
+              TextSpan(text: 'pts', style: TextStyle(fontWeight: FontWeight.bold)),
+            ],
+          ),
+        ),
+        Text(humanize(stopwatch)),
+        Text(param.game),
+        SizedBox( 
+          width: 50,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Image.asset(
+                AppIcons.check,
+                height: 25,
+                width: 25,
+              ),
+              Text(param.score.perfectRounds.toString())
+            ],
+          )
+        ),
+        SizedBox(
+          width: 50,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Image.asset(
+                AppIcons.wrong,
+                height: 25,
+                width: 25,
+              ),
+              Text(param.score.wrongAttempts.toString())
+            ],
           )
         )
-      )
+      ],
     );
   }
+
 }
