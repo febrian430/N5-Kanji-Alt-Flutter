ce6a8dd01f9f560b72f9b3e9f73120e9d59f393d
Febrian <febrian430@gmail.com>
2022-01-10 11:58:21 +0700

    delete deps

--- lib/components/submit_button.dart
+++ lib/components/submit_button.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:kanji_memory_hint/menu_screens/result_screen.dart';
-import 'package:kanji_memory_hint/route_param.dart';
 
 class SubmitButton extends StatelessWidget  {
   const SubmitButton({Key? key, required this.visible, required this.onTap}) : super(key: key);
