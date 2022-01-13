766c570924b79be37d1a75192ac9150eb87f81b1
Febrian <febrian430@gmail.com>
2022-01-13 20:17:25 +0700

    pick drop repo fix

--- lib/pick-drop/repo.dart
+++ lib/pick-drop/repo.dart
@@ -8,28 +8,29 @@ class PickDropQuestionMaker {
   static Future<List<QuestionSet>> makeQuestionSet(int n, int chapter, GAME_MODE mode) async {
     var kanjis = await SQLRepo.gameQuestions.byChapterForQuestion(chapter, n, 1/2, false);
     List<QuestionSet> questionSets = [];
+    var optionKanjis = await SQLRepo.gameQuestions.byChapter(chapter);
 
     for (var kanji in kanjis) {
-      questionSets.add(await _imageMeaning(kanji));
+      questionSets.add(await _imageMeaning(kanji, optionKanjis));
     }
     return questionSets;
   }
 
-  static Future<QuestionSet> _imageMeaning(Example kanji) async {
-    List<Option> options = await _findOptionsFor(8, kanji, GAME_MODE.imageMeaning);
+  static Future<QuestionSet> _imageMeaning(Example kanji, List<Example> optionKanjis) async {
+    List<Option> options = await _findOptionsFor(8, kanji, GAME_MODE.imageMeaning, optionKanjis);
 
     Question question = Question(value: kanji.image, key: kanji.id.toString(), isImage: true);
     return QuestionSet(question: question, options: options);
   }
 
 
-  static Future<List<Option>> _findOptionsFor(int n, Example question, GAME_MODE mode) async {
+  static Future<List<Option>> _findOptionsFor(int n, Example question, GAME_MODE mode, List<Example> optionKanjis) async {
     List<Option> options;
 
-    var kanjis = await SQLRepo.gameQuestions.random(n: n-1, startChapter: question.chapter-1, endChapter: question.chapter+1);
-    var otherOptions = kanjis.where((kanji) => kanji.id != question.id).toList();
+    var otherOptions = optionKanjis.where((kanji) => kanji.id != question.id).toList();
     otherOptions.shuffle();
     otherOptions = otherOptions.take(n-1).toList();
+    
     if(mode == GAME_MODE.imageMeaning) {
       options = [
         Option(value: question.rune, key: question.id.toString()),
